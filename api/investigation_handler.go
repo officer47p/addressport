7393deeb9b55d44fd1eacf2cf4453ea7dd3fc007
Parsa Hosseini <officer47p@gmail.com>
2024-07-07 02:58:50 +0330

    Removing empty children list from response

--- api/investigation_handler.go
+++ api/investigation_handler.go
@@ -11,9 +11,30 @@ type InvestigationHandler struct {
 	explorer explorer.Explorer
 }
 
+func NewInvestigationHandler(explorer explorer.Explorer) *InvestigationHandler {
+	return &InvestigationHandler{explorer: explorer}
+}
+
+func (h *InvestigationHandler) HandleGetAssociatedAddresses(c *fiber.Ctx) error {
+	address := c.Params("address")
+	depthString := c.Query("depth", "1")
+	depth, err := strconv.Atoi(depthString)
+	if err != nil {
+		return err
+	}
+
+	addressNode := AddressNode{Address: address, Children: []*AddressNode{}}
+	if err = addressNode.PopulateNode(depth, h.explorer); err != nil {
+		return err
+	}
+
+	return c.JSON(addressNode)
+
+}
+
 type AddressNode struct {
 	Address  string
-	Children []*AddressNode
+	Children []*AddressNode `json:"children,omitempty"`
 }
 
 func (n *AddressNode) PopulateNode(depth int, exp explorer.Explorer) error {
@@ -69,62 +90,4 @@ func getAssociatedAddressesForAddress(address string, exp explorer.Explorer) ([]
 	}
 
 	return addresses, nil
-}
-func NewInvestigationHandler(explorer explorer.Explorer) *InvestigationHandler {
-	return &InvestigationHandler{explorer: explorer}
-}
-
-func (h *InvestigationHandler) HandleGetAssociatedAddresses(c *fiber.Ctx) error {
-	address := c.Params("address")
-	depthString := c.Query("depth", "1")
-	depth, err := strconv.Atoi(depthString)
-	if err != nil {
-		return err
-	}
-
-	addressNode := AddressNode{Address: address, Children: []*AddressNode{}}
-	addressNode.PopulateNode(depth, h.explorer)
-
-	return c.JSON(addressNode)
-
-	// txs, err := h.explorer.GetAllTransactionsForAddress(address)
-	// if err != nil {
-	// 	return err
-	// }
-	// if len(txs) == 0 {
-	// 	return c.SendStatus(404)
-	// }
-
-	// addresses := map[string]bool{}
-
-	// for _, tx := range txs {
-	// 	from := tx.From
-	// 	to := tx.To
-
-	// 	if from != address {
-	// 		addresses[from] = true
-	// 	}
-	// 	if to != address {
-	// 		addresses[to] = true
-
-	// 	}
-	// }
-
-	// return c.JSON(addresses)
-
-	// for txn in txn_data['result']:
-	//         from_address = txn['from']
-	//         to_address = txn['to']
-
-	//         # Check if 'from' address is flagged
-	//         if from_address != address:
-	//             from_flagged = check_address_flagged(from_address)
-	//             if from_flagged and from_address not in scam_interactions:
-	//                 scam_interactions.append(from_address)
-
-	//         # Check if 'to' address is flagged
-	//         if to_address != address:
-	//             to_flagged = check_address_flagged(to_address)
-	//             if to_flagged and to_address not in scam_interactions:
-	//                 scam_interactions.append(to_address)
 }
